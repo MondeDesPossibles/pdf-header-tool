8c99b0d9f7b025825bda4c9ce9842f0dc9d1b18a
gcomble <198053591+gcomble@users.noreply.github.com>
2026-02-22 13:28:34 +0100

    chore: bump version v0.4.6.8

--- pdf_header.py
+++ pdf_header.py
@@ -5,7 +5,7 @@
 # Repo    : MondeDesPossibles/pdf-header-tool
 # ==============================================================================
 
-VERSION     = "0.4.6.7"
+VERSION     = "0.4.6.8"
 BUILD_ID    = "build-2026.02.22.01"
 GITHUB_REPO = "MondeDesPossibles/pdf-header-tool"
 
